9dd91976535563e197ac4a5e1d1dd944ee507b7c
Jeremy Curtis <weyoz.dev@gmail.com>
2016-01-05 21:30:06 +0100

    victory and defeat page added

---
+++ app/src/main/java/com/example/manuel/x_wingsescape/DefeatActivity.java
@@ -0,0 +1,13 @@
+package com.example.manuel.x_wingsescape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DefeatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defeat);
+    }
+}
