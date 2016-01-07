07b04c9e5b06d2da71e550e660bcf64e68c5fcbf
Jeremy Curtis <weyoz.dev@gmail.com>
2016-01-07 16:31:08 +0100

    deplacement accurate still bug

--- app/src/main/java/com/example/manuel/x_wingsescape/GameActivity.java
+++ app/src/main/java/com/example/manuel/x_wingsescape/GameActivity.java
@@ -189,19 +189,19 @@ public class GameActivity extends AppCompatActivity {
                             || Vecteur[1] == ((3 * Math.PI) / 4)
                             || Vecteur[1] == ((-3 * Math.PI) / 4)){
 
-                        Vecteur[1] += 0.05;
+                        Vecteur[1] += 0.5;
                     }
 
-                    if (Vecteur[1] > (-Math.PI / 4) && Vecteur[1] < (Math.PI / 4)){
+                    if (((Vecteur[1] > (-Math.PI / 4)) && (Vecteur[1] <= 0)) || ((Vecteur[1] < (Math.PI / 4)) && (Vecteur[1] >= 0))){
                         grid[x + 1][y] = grid[x + 1][y] + grid[x][y];
                         grid[x][y] = 0;
-                    } else if (Vecteur[1] > ((-3 * Math.PI) / 4) && Vecteur[1] < (-Math.PI / 4)){
+                    } else if ((Vecteur[1] > ((-3 * Math.PI) / 4)) && (Vecteur[1] < (-Math.PI / 4))){
                         grid[x][y - 1] = grid[x][y - 1] + grid[x][y];
                         grid[x][y] = 0;
-                    } else if (Vecteur[1] > ((3 * Math.PI) / 4) && Vecteur[1] < ((-3 * Math.PI) / 4)){
+                    } else if (((Vecteur[1] > ((3 * Math.PI) / 4)) && (Vecteur[1] <= Math.PI)) || ((Vecteur[1] < ((-3 * Math.PI) / 4)) && (Vecteur[1] >= (-Math.PI)))){
                         grid[x - 1][y] = grid[x - 1][y] + grid[x][y];
                         grid[x][y] = 0;
-                    } else if (Vecteur[1] > ( Math.PI / 4) && Vecteur[1] < ((3 * Math.PI) / 4)){
+                    } else if ((Vecteur[1] > ( Math.PI / 4)) && (Vecteur[1] < ((3 * Math.PI) / 4))){
                         grid[x][y + 1] = grid[x][y + 1] + grid[x][y];
                         grid[x][y] = 0;
                     }
